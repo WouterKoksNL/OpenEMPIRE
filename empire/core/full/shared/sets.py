a84479747cf8ce7a6e1e2135978f704bef1c74cc
WouterKoksNL <wouterkoks135@live.nl>
2025-11-10 15:36:37 +0100

    offshore conv

--- empire/core/full/shared/sets.py
+++ empire/core/full/shared/sets.py
@@ -3,8 +3,9 @@ from empire.core.empire_types import Flags
 from .base.sets import define_base_shared_sets, load_base_shared_set_data, define_base_shared_derived_sets
 from .heat.sets import define_heat_sets, define_heat_derived_sets, load_heat_set_data
 from .hydrogen.sets import define_hydrogen_sets, define_hydrogen_derived_sets, load_hydrogen_set_data
-
 from .industry.sets import define_industry_sets, define_industry_derived_sets, load_industry_set_data
+from .offshore_converters.sets import define_shared_offshore_converter_derived_sets
+
 
 def define_shared_sets(
         model, 
@@ -26,7 +27,6 @@ def define_shared_sets(
 
 
 def load_shared_set_data(data, dataset_dir, model, flags: Flags, load_period, periods_active):
-    print("NOT SURE IF SHOULD LOAD PERIOD HERE")
     load_base_shared_set_data(data, dataset_dir, model, load_period=load_period, periods_active=periods_active)
     
     # Load module-specific set data (conditional)
@@ -42,8 +42,13 @@ def load_shared_set_data(data, dataset_dir, model, flags: Flags, load_period, pe
 
 
 def define_shared_derived_sets(model, offshoreNodesList, flags: Flags):
-    define_base_shared_derived_sets(model, offshoreNodesList)
-    # Build derived sets that depend on loaded data
+    """Build derived sets that depend on loaded data"""
+    
+    define_base_shared_derived_sets(model)
+
+    if flags.offshore_converters:
+        define_shared_offshore_converter_derived_sets(model, offshoreNodesList)
+
     if flags.heat:
         define_heat_derived_sets(model)
 
