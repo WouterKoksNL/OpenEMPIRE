ab76f088812f301900eb6a30ea250cde074d330c
WouterKoksNL <wouterkoks135@live.nl>
2025-11-12 16:47:45 +0100

    Update out_of_sample.py

--- empire/core/full/investment/base/out_of_sample.py
+++ empire/core/full/investment/base/out_of_sample.py
@@ -1,27 +1,37 @@
 from pyomo.environ import Param, NonNegativeReals
 
+from empire.core.loading_utils import load_params
 
-def define_base_investments_as_param(model):
+
+def define_base_investments_as_parameters(model, set_only_capacities: bool = False):
     # Redefine investment vars as input parameters
+    model.genInstalledCap = Param(model.GeneratorsOfNode, model.Period, domain=NonNegativeReals, mutable=True)
+    model.transmissionInstalledCap = Param(model.BidirectionalArc, model.Period, domain=NonNegativeReals, mutable=True)
+    model.storPWInstalledCap = Param(model.StoragesOfNode, model.Period, domain=NonNegativeReals, mutable=True)
+    model.storENInstalledCap = Param(model.StoragesOfNode, model.Period, domain=NonNegativeReals, mutable=True)
+    if set_only_capacities: 
+        return 
     model.genInvCap = Param(model.GeneratorsOfNode, model.Period, domain=NonNegativeReals)
     model.transmissionInvCap = Param(model.BidirectionalArc, model.Period, domain=NonNegativeReals)
     model.storPWInvCap = Param(model.StoragesOfNode, model.Period, domain=NonNegativeReals)
     model.storENInvCap = Param(model.StoragesOfNode, model.Period, domain=NonNegativeReals)
-    model.genInstalledCap = Param(model.GeneratorsOfNode, model.Period, domain=NonNegativeReals)
-    model.transmissionInstalledCap = Param(model.BidirectionalArc, model.Period, domain=NonNegativeReals)
-    model.storPWInstalledCap = Param(model.StoragesOfNode, model.Period, domain=NonNegativeReals)
-    model.storENInstalledCap = Param(model.StoragesOfNode, model.Period, domain=NonNegativeReals)
-    
+    return 
 
-def load_base_oos_investments(model, data, result_file_path):
-    """Load investment decisions from in-sample runs as parameters for out-of-sample runs."""
-    # Optimized investment decisions read from result file from in-sample runs
-    data.load(filename=str(result_file_path + "/" + 'genInvCap.tab'), param=model.genInvCap, format="table")
-    data.load(filename=str(result_file_path + "/" + 'transmissionInvCap.tab'), param=model.transmissionInvCap, format="table")
-    data.load(filename=str(result_file_path + "/" + 'storPWInvCap.tab'), param=model.storPWInvCap, format="table")
-    data.load(filename=str(result_file_path + "/" + 'storENInvCap.tab'), param=model.storENInvCap, format="table")
-    data.load(filename=str(result_file_path + "/" + 'genInstalledCap.tab'), param=model.genInstalledCap, format="table")
-    data.load(filename=str(result_file_path + "/" + 'transmissionInstalledCap.tab'), param=model.transmissionInstalledCap, format="table")
-    data.load(filename=str(result_file_path + "/" + 'storPWInstalledCap.tab'), param=model.storPWInstalledCap, format="table")
-    data.load(filename=str(result_file_path + "/" + 'storENInstalledCap.tab'), param=model.storENInstalledCap, format="table")
 
+def load_base_optimized_investments(model, data, result_file_path, set_only_capacities: bool = False):
+    """Optimized investment decisions read from result file from in-sample runs"""
+    investment_params = [
+        "genInstalledCap",
+        "transmissionInstalledCap",
+        "storPWInstalledCap",
+        "storENInstalledCap",
+    ]
+    if not set_only_capacities:
+        investment_params += [
+            "genInvCap",
+            "transmissionInvCap",
+            "storPWInvCap",
+            "storENInvCap",
+        ]
+    load_params(data, model, result_file_path, investment_params, component="investments")  
+    return 
